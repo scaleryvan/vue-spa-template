c484efa5db6f4fa8b606996577e283f0e375fa24
yefan <scaleryvan@gmail.com>
2018-12-18 20:09:35 +0800

    add custom code split

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+	configureWebpack: {
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					vendors: {
+						test: /[\\/]node_modules[\\/]/,
+						priority: -10,
+						chunks: 'all'
+					},
+					vue: {
+						test: /[\\/]node_modules[\\/](vue)[\\/]/,
+						name: 'vue',
+						priority: 10,
+						chunks: 'all'
+					}
+				}
+			}
+		}
+	},
 	css: {
 		loaderOptions: {
 			// 给 sass-loader 传递选项
